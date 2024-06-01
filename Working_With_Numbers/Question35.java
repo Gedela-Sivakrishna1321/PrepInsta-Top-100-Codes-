11caa7161c797829af1e0c7af33a0d2c9270d2a8
sivakrishna <gedelashipa.1321@gmail.com>
2024-06-01 19:57:34 +0530

    Day8

---
+++ Working_With_Numbers/Question35.java
@@ -0,0 +1,36 @@
+package Working_With_Numbers;
+import java.util.Scanner;
+// Convert Decimal Number to Binary Number - 12  ==> 1100
+     //                                       (10)      (2)
+
+/*      Approach :- 
+ *          1) Loop till num >= 2
+ *          2) Find out the reminder when dividing number by 2
+ *          3) Add the remainder to the result
+ *          4) Update the number i.e num = num / 2
+ */
+
+public class Question35 {
+
+    public static String binaryNum(int num) {
+
+        StringBuilder result = new StringBuilder();
+
+        while(num != 0) {
+            int remainder = num % 2;
+            result.append(remainder);
+            num = num / 2;
+        }
+
+        return result.reverse().toString();
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int decNum = sc.nextInt();
+
+        System.out.println("Binary represented number = " + binaryNum(decNum));
+
+        sc.close();
+    }
+}
---
+++ Working_With_Numbers/Question36.java
@@ -0,0 +1,40 @@
+package Working_With_Numbers;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+// Convert Decimal to Octal - 12     ==> 14
+//                              (10)      (8)
+
+/*      Approach :- 
+ *          1) Approach is same as Decimal to Binary Convertion, just divide by 8
+ */
+
+public class Question36 {
+
+    public static String octalNum(BigInteger num) {
+
+        StringBuilder result = new StringBuilder();
+        
+        while(num.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger remainder = num.mod(BigInteger.valueOf(8));
+            result.append(remainder);
+            num.divide(BigInteger.valueOf(8));
+        }
+
+        return result.reverse().toString();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger decNum = sc.nextBigInteger();
+
+        // System.out.println(!decNum.equals(0));
+        
+        String octalString = decNum.toString(8);
+        System.out.println("Octal Represented : " + octalString);
+        
+        sc.close();
+    }
+    
+}
